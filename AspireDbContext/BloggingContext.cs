b33044990e45266b94d332baa36900f9d59eb4ec
Dejan Appenzeller <dejan.appenzeller@swisspeers.ch>
2024-09-04 14:09:30 +0200

    have a working DbContext with local SQL Server

---
+++ AspireDbContext/BloggingContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspireDbContext
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer($"Server=localhost;Database=repro-aspire-dbcontext;Trusted_Connection=True;MultipleActiveResultSets=True;Encrypt=False");
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
