b476843d5ea3d1f5e7853af6becd58e0e5d31997
Dejan Appenzeller <dejan.appenzeller@swisspeers.ch>
2024-09-04 14:42:56 +0200

    update DB in separate scopes

--- AspireDbContext/Program.cs
+++ AspireDbContext/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using AspireDbContext;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,29 +11,64 @@ builder.Services.AddDbContext<BloggingContext>();
 using IHost host = builder.Build();
 host.Start();
 
-using var scope = host.Services.CreateScope();
-using var db = scope.ServiceProvider.GetRequiredService<BloggingContext>();
-
-db.Database.EnsureCreated();
+ExecuteDb(db => db.Database.EnsureCreated());
 
 // Create
 Console.WriteLine("Inserting a new blog");
+ExecuteDb(db =>
+{
 db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
 db.SaveChanges();
+});
 
 // Read
-Console.WriteLine("Querying for a blog");
-var blog = db.Blogs
-    .OrderBy(b => b.BlogId)
-    .First();
+ListBlogs();
 
 // Update
 Console.WriteLine("Updating the blog and adding a post");
-blog.Url = "https://devblogs.microsoft.com/dotnet";
-blog.Posts.Add(new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
-db.SaveChanges();
+ExecuteDb(db =>
+{
+    var blog = db.Blogs
+        .OrderBy(b => b.BlogId)
+        .First();
+    blog.Url = "https://devblogs.microsoft.com/dotnet";
+    blog.Posts.Add(new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+    db.SaveChanges();
+});
+
+ListBlogs();
+
 
 // Delete
 Console.WriteLine("Delete the blog");
-db.Remove(blog);
-db.SaveChanges();
+ExecuteDb(db =>
+{
+    var blog = db.Blogs
+        .OrderBy(b => b.BlogId)
+        .First();
+    db.Remove(blog);
+    db.SaveChanges();
+});
+
+ListBlogs();
+
+
+void ListBlogs()
+{
+    Console.WriteLine("List blogs");
+    ExecuteDb(db =>
+    {
+        foreach (var blog in db.Blogs
+            .OrderBy(b => b.BlogId))
+        {
+            Console.WriteLine("Found {0}: {1} ({2} posts)", blog.BlogId, blog.Url, blog.Posts.Count);
+        }
+    });
+}
+
+void ExecuteDb(Action<BloggingContext> action)
+{
+    using var scope = host.Services.CreateScope();
+    using var db = scope.ServiceProvider.GetRequiredService<BloggingContext>();
+    action(db);
+}
